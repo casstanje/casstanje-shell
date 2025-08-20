cbaedef081b3adc1b07ecaad0fe6ed3e78d92ef6
casstanje <casstanje@pm.me>
2025-08-20 16:56:42 +0200

    allow for empty keyboard layout (use default)

--- setup.sh
+++ setup.sh
@@ -267,17 +267,19 @@ else
         cp -r dotfiles/.config/. "$HOME"/.config/
 
         echo
-        echo "Preferred keyboard layout (country code, e.g. 'gb' for great britain, 'de' for german, 'us' for united states)"
+        echo "Preferred keyboard layout (leave empty to use the default. country code, e.g. 'gb' for great britain, 'de' for german, 'us' for united states)"
         chosenKeyboardLayout=1
         countryCodeRegex='\b(ad|ae|af|ag|ai|al|am|ao|aq|ar|as|at|au|aw|ax|az|ba|bb|bd|be|bf|bg|bh|bi|bj|bl|bm|bn|bo|bq|br|bs|bt|bv|bw|by|bz|ca|cc|cd|cf|cg|ch|ci|ck|cl|cm|cn|co|cr|cu|cv|cw|cx|cy|cz|de|dj|dk|dm|do|dz|ec|ee|eg|eh|er|es|et|fi|fj|fk|fm|fo|fr|ga|gb|gd|ge|gf|gg|gh|gi|gl|gm|gn|gp|gq|gr|gs|gt|gu|gw|gy|hk|hm|hn|hr|ht|hu|id|ie|il|im|in|io|iq|ir|is|it|je|jm|jo|jp|ke|kg|kh|ki|km|kn|kp|kr|kw|ky|kz|la|lb|lc|li|lk|lr|ls|lt|lu|lv|ly|ma|mc|md|me|mf|mg|mh|mk|ml|mm|mn|mo|mp|mq|mr|ms|mt|mu|mv|mw|mx|my|mz|na|nc|ne|nf|ng|ni|nl|no|np|nr|nu|nz|om|pa|pe|pf|pg|ph|pk|pl|pm|pn|pr|ps|pt|pw|py|qa|re|ro|rs|ru|rw|sa|sb|sc|sd|se|sg|sh|si|sj|sk|sl|sm|sn|so|sr|ss|st|sv|sx|sy|sz|tc|td|tf|tg|th|tj|tk|tl|tm|tn|to|tr|tt|tv|tw|tz|ua|ug|um|us|uy|uz|va|vc|ve|vg|vi|vn|vu|wf|ws|ye|yt|za|zm|zw)\b'
         while [ $chosenKeyboardLayout == 1 ] 
         do
             read -r keyboardLayout
-            if [[ $keyboardLayout =~ $countryCodeRegex ]]; then
-                sed -i -e "s/kb_layout = /kb_layout = $keyboardLayout/g" $HOME/.config/hypr/hyprinput.conf
-                chosenKeyboardLayout=0
-            else
-                echo "Country code not valid. Try again."
+            if [ -n "$keyboardLayout" ]; then
+                if [[ $keyboardLayout =~ $countryCodeRegex ]]; then
+                    sed -i -e "s/kb_layout = /kb_layout = $keyboardLayout/g" $HOME/.config/hypr/hyprinput.conf
+                    chosenKeyboardLayout=0
+                else
+                    echo "Country code not valid. Try again."
+                fi
             fi
         done
     fi
